6575f5b34c81b22eab7ffda8d31eac848d21e00b
Daniel Brad <danieltbrad@gmail.com>
2016-09-16 16:56:42 -0700

    created daily report show page; created nap_duration method in daily_report.rb

--- app/models/daily_report.rb
+++ app/models/daily_report.rb
@@ -1,3 +1,9 @@
 class DailyReport < ApplicationRecord
   belongs_to :child
+
+  def nap_duration
+    # distance_of_time_in_words(nap_end - nap_start)
+    Time.at(nap_end - nap_start).utc.strftime("%-H:%M") #=> "01:00:00"
+  end
+
 end
