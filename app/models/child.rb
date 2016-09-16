1a3a820781f176b0b1c36c65560cce86136b53fe
Daniel Brad <danieltbrad@gmail.com>
2016-09-16 13:52:12 -0700

    added child form validations

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,8 +1,18 @@
 class Child < ApplicationRecord
   has_many :daily_reports, dependent: :destroy
+  validates :first_name, presence: { message: "You must enter a first name" }
+  validates :last_name, presence: { message: "You must enter a last name" }
+  validates :last_name, uniqueness: {scope: :first_name, message: "A child with that name is already enrolled" }
+  validate :proper_age
+
+  def proper_age
+    if (DateTime.now - birthdate < 365)
+      errors.add(:birthdate, "Children must be at least one year old to enroll")
+    end
+  end
 
   def name
     self.first_name + " " +self.last_name
   end
-  
+
 end
