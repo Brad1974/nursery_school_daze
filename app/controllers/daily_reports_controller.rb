1f7bf26f2e38906b6ba94304ddd58dd4f3e841aa
Daniel Brad <danieltbrad@gmail.com>
2016-09-16 13:59:25 -0700

    added CRUD actions to daily_reports_controller.rb and blank show page

--- app/controllers/daily_reports_controller.rb
+++ app/controllers/daily_reports_controller.rb
@@ -1,2 +1,54 @@
 class DailyReportsController < ApplicationController
+  before_action :set_child
+
+  def show
+    @daily_report = @child.daily_reports.find(params[:id])
+  end
+
+  def index
+    redirect_to child_path(@child)
+  end
+
+  def new
+    @daily_report = @child.daily_reports.build
+  end
+
+  def create
+    @daily_report = @child.daily_reports.build(daily_report_params)
+    if @daily_report.save
+      redirect_to child_daily_report_path(@child, @daily_report), notice: "report generated"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @daily_report = @child.daily_reports.find(params[:id])
+  end
+
+  def update
+    @daily_report = @child.daily_reports.find(params[:id])
+    if @daily_report.update(daily_report_params)
+      @daily_report.save
+      redirect_to child_daily_report_path(@child, @daily_report), notice: "report updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @child.daily_reports.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def daily_report_params
+    params.require(:daily_report).permit(:child_id, :date, :wet_diapers, :poopy_diapers, :morning_snack, :lunch, :afternoon_snack, :clothing_needed, :need_diapers, :other_needed, :nap_start :nap_end, :narrative)
+  end
+
+
+  def set_child
+    @child = Child.find(params[:child_id])
+  end
 end
