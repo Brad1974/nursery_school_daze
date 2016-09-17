a3659d1cd513a82e8334273a3d8f4fb64c99a398
Daniel Brad <danieltbrad@gmail.com>
2016-09-17 06:19:44 -0700

    refactored daily_reports.js

--- app/assets/javascripts/daily_reports.js
+++ app/assets/javascripts/daily_reports.js
@@ -1,11 +1,24 @@
 $(document).on("turbolinks:load", function(){
-  // $('.nap').val("select")
-  $('.nap').timepicker({ 'timeFormat': 'g:ia', 'disableTextInput' : true });
-  $('.nap').timepicker('option', {'minTime': '1:00pm', 'maxTime': '5:00pm', 'step': '5' } );
 
-  $('#nap_check').on('change', function(){
-    // $('.nap').val("select")
-    $('#nap_entry').toggle();
-  })
+  setInput();
+  setUpTimePicker();
+  toggleNapFields();
 
 });
+
+function setInput(){
+  if ( $('.nap:last').val() === $('.nap:first').val() )
+    {$('.nap').val("select")};
+  };
+
+function setUpTimePicker(){
+  $('.nap').timepicker({ 'timeFormat': 'g:ia', 'disableTextInput' : true });
+  $('.nap').timepicker('option', {'minTime': '1:00pm', 'maxTime': '5:00pm', 'step': '5' } );
+}
+
+function toggleNapFields(){
+  $('#nap_check').on('change', function(){
+    $('.nap').val("select")
+    $('#nap_entry').toggle();
+  })
+}
