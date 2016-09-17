54263b7533f3cb7ba10b321a3d6676cd34c3a7c8
Daniel Brad <danieltbrad@gmail.com>
2016-09-16 20:01:09 -0700

    added needs to daily report show page; needs method to daily_report.rb

--- app/assets/javascripts/daily_reports.js
+++ app/assets/javascripts/daily_reports.js
@@ -1,5 +1,6 @@
 $(document).on("turbolinks:load", function(){
-$('h4').css('color', 'red');
-$('.nap').timepicker({ 'timeFormat': 'g:ia' });
-$('.nap').timepicker('option',{'minTime': '1:00pm', 'maxTime': '5:00pm', 'step': '5'} );
+
+  $('.nap').timepicker({ 'timeFormat': 'g:ia' });
+  $('.nap').timepicker('option',{'minTime': '1:00pm', 'maxTime': '5:00pm', 'step': '5'} );
+
 });
--- app/models/daily_report.rb
+++ app/models/daily_report.rb
@@ -2,8 +2,17 @@ class DailyReport < ApplicationRecord
   belongs_to :child
 
   def nap_duration
-    # distance_of_time_in_words(nap_end - nap_start)
-    Time.at(nap_end - nap_start).utc.strftime("%-H:%M") #=> "01:00:00"
+    if nap_start && nap_end
+      Time.at(nap_end - nap_start).utc.strftime("%-H:%M")
+    end
+  end
+
+  def needs
+    needs_array = []
+    needs_array << self.clothing_needed if !self.clothing_needed.empty?
+    needs_array << self.other_needed if !self.other_needed.empty?
+    needs_array << "diapers" if self.need_diapers
+    return needs_array
   end
 
 end
