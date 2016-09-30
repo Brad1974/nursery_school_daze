258cf4f722c1aaafe326013b15d89b47c0bae79a
Daniel Brad <danieltbrad@gmail.com>
2016-09-30 11:04:38 -0700

    incorporated cocoon to dynamically generate child fields in new user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :children
+  accepts_nested_attributes_for :children, reject_if: :all_blank, allow_destroy: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
